203e342007517abc06e4bd3215a5fba01dcde6bb
Stefan Freitag <stefan.freitag@udo.edu>
2020-10-16 21:40:52 +0200

    feat: added ec2 demo code

---
+++ terraform-ec2-demo/main.tf
@@ -0,0 +1,68 @@
+provider "aws" {
+  version = "3.11.0"
+  region  = "eu-central-1"
+  profile = "cdk"
+
+}
+
+variable "key_name" {
+  description = "Key pair name for instance."
+  default     = ""
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_security_group" "hello_world" {
+  name_prefix = "hello-world-"
+  description = "Hello world security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "hello_world" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.hello_world.id]
+  user_data              = <<EOF
+#cloud-config
+packages:
+  - nginx
+EOF
+  tags = {
+    Name = "hello-world"
+  }
+}
