66244018f588c120845ae250f01708a21959ba2f
Stefan Freitag <stefan.freitag@udo.edu>
2021-02-26 10:47:19 +0100

    feat: added remote state on s3 code

---
+++ learn-terraform-remote-state/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "cdk"
+  region  = "us-west-2"
+}
+
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "sf-terraform-up-and-running-state"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "sf-terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
