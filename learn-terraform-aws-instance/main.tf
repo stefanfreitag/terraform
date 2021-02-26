1243abc42da7b3bcd98f17eee2446a3bd7e4b78e
Stefan Freitag <stefan.freitag@udo.edu>
2021-02-26 10:48:05 +0100

    feat: added example for aws instance

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,23 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "cdk"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0d65e3b43fa4a7ed9"
+  tags = {
+    Name = var.instance_name
+  }
+}
